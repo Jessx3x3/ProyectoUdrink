2977ed9b12ba54b07dd7e33e25591fbd66845def
Jessx3x3 <j.vasquez12@ufromail.cl>
2017-10-25 17:23:23 -0300

    Botón mayor de edad ready for life, bien fino y elegante pal mundo mundial de pana hermano blees, bendicione

--- app/src/main/java/com/example/pruebalanzadora/vuelvetealcocteludrink/PrincipalActivity.java
+++ app/src/main/java/com/example/pruebalanzadora/vuelvetealcocteludrink/PrincipalActivity.java
@@ -1,15 +1,35 @@
 package com.example.pruebalanzadora.vuelvetealcocteludrink;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    Button siguiente;
+    Button no;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        siguiente = (Button)findViewById(R.id.btnSi);
+        no = (Button)findViewById(R.id.btnNo);
+        siguiente.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+                public void onClick(View w){
+
+                Intent siguiente = new Intent(PrincipalActivity.this, Cocteles.class);
+                startActivity(siguiente);
+
+            }
+        });
+
     }
 
 }
